20e752bc01e0602953e9ca6c3ef03f15b96f7a2e
Sourov Ray <sourov7077@gmail.com>
2026-06-22 21:39:30 +0600

    Add jokes URL route to portfolio

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('projects/', views.project_list, name='projects'),
     path('projects/<int:pk>/', views.project_detail, name='project_detail'),
     path('contact/', views.contact, name='contact'),
+    path('jokes/', views.jokes, name='jokes'),
 ]
